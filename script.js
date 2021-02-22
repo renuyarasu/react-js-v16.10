51b5d8c5b16d9b125abb8b148bdd5aad444243d0
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-02-22 19:34:19 +0530

    Class 02 [OPERATORS]

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-/* Class 01 [1.9.21] */
+/* Class 02 [OPERATORS] */
 
-var x = 1;
+// Arithmetic Operators
+
+var x = 2;
 var y = 2;
-var z = null;
-console.log(x + y);
 
-// Types of errors
-// syntax
-// reference
-// type
+// var z = ('x + y = ', x + y);
+// var z = ('x - y = ', x - y);
+// var z = ('x * y =', x * y);
+// var z = ('x/y = ', x / y);
+// var z = ('x % y =', x % y);
+// var z = ('++x = ', ++x);
+// var z = ('--x =', --x);
+// var z = ('x =', x);
+// var z = ('x ** y =', x ** y);
+// var z = true && true;
+// var z = true && false;
+// var z = false && false;
+// var z = true || true;
+// var z = true || false;
+var z = false || false;
+
+console.log(z);
+
+// Comparison Operators
+// Logical (or Relational) Operators
+
+// var a = 100;
+// var b = 200;
+// var c = 100;
+
+// var x = {} && 200;
+
+// console.log(x);
+
+// Assignment Operators
+// Conditional (or ternary) Operators
+
+// var a = 100;
+// var b = 'hindu';
+// var c = '500';
+
+// var x = ;
+
+// console.log(x);
