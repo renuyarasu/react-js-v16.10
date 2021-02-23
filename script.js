e63f2e3f52005ab0d20f9257e96cc84e067bac7f
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-02-23 18:41:19 +0530

    getHours

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-/* Class 02 [OPERATORS] */
-
-// Arithmetic Operators
-
-var x = 2;
-var y = 2;
-
-// var z = ('x + y = ', x + y);
-// var z = ('x - y = ', x - y);
-// var z = ('x * y =', x * y);
-// var z = ('x/y = ', x / y);
-// var z = ('x % y =', x % y);
-// var z = ('++x = ', ++x);
-// var z = ('--x =', --x);
-// var z = ('x =', x);
-// var z = ('x ** y =', x ** y);
-// var z = true && true;
-// var z = true && false;
-// var z = false && false;
-// var z = true || true;
-// var z = true || false;
-var z = false || false;
-
-console.log(z);
-
-// Comparison Operators
-// Logical (or Relational) Operators
-
-// var a = 100;
-// var b = 200;
-// var c = 100;
-
-// var x = {} && 200;
-
-// console.log(x);
-
-// Assignment Operators
-// Conditional (or ternary) Operators
-
-// var a = 100;
-// var b = 'hindu';
-// var c = '500';
-
-// var x = ;
-
-// console.log(x);
+/* Class 03 [ if/else in JavaScript ] */
+var hours = new Date().getHours();
+if (hours < 12) {
+  console.log('Good Morning!');
+} else {
+  console.log('Good AN');
+}
